ec72578c550bdbb99ec621e6f2157873730d3c19
Arjun <61193215+ArjunR00T@users.noreply.github.com>
2021-09-17 11:48:06 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,9 @@
+print("Hello rashad monusss")
+print("Python program to add numbers")
+a=int(input("enter 1st number "))
+b=int(input("enter 2nd number "))
+print("Sum= ",a+b)
+print("product=",a*b)
+print("Devision=",a/b)
+print("Subtraction=",a-b)
+
