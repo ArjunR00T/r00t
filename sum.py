69f02a65fd572085d6c1b53a02657d74f7bbfadd
Arjun <61193215+ArjunR00T@users.noreply.github.com>
2021-09-17 12:04:00 +0530

    Delete sum.py

--- sum.py
+++
@@ -1,9 +0,0 @@
-
-print("Python program to add numbers")
-a=int(input("enter 1st number "))
-b=int(input("enter 2nd number "))
-print("Sum= ",a+b)
-print("product=",a*b)
-print("Devision=",a/b)
-print("Subtraction=",a-b)
-
